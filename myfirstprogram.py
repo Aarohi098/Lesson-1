a69a6affa99e4e9de134fca862ebc55263053b2f
Aarohi098 <aarohimahajan007@gmail.com>
2025-10-13 18:48:01 +0100

    Lesson 1 complete

--- myfirstprogram.py
+++ myfirstprogram.py
@@ -1,4 +1,15 @@
 # The printing statement is showing my name
-print("Aarohi")
+print("\n Aarohi")
 # The printing statement is showing my school name
-print("Upton Court Grammar")
+print("Upton Court")
+
+name = input("Enter your name:") #string
+print("Hello", name,", Welcome to the World of Python Programming")
+print(6)
+
+variable = 56.32 #float
+variable1 = 34 #integer
+
+print("The data type of name is:", type(name))
+print("The data type of name is:", type(variable))
+print("The data type of name is:", type(variable1))
